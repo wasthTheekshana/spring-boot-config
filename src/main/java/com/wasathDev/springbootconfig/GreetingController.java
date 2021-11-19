e602848382cc13e20f6ddb431823daafeefb4673
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2021-11-19 20:02:25 +0530

    three @value tricks

--- src/main/java/com/wasathDev/springbootconfig/GreetingController.java
+++ src/main/java/com/wasathDev/springbootconfig/GreetingController.java
@@ -4,14 +4,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: defualt value}") //deafult message if prop file down
     private String greetingMeesage;
+
+    @Value("gfghjhjhv hjg jhg")
+    private String staticMessage;
+
+    @Value("${my.list.values}") //list example
+    private List<String> listValue;
+
+    @Value("#{${dbValues}}") //map
+    private Map<String, String> dbVaules;
+
 
     @GetMapping("/greeting")
     public String greeting(){
-        return greetingMeesage;
+        return greetingMeesage + staticMessage + listValue +dbVaules;
     }
 }
